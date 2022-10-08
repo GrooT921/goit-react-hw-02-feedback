87814e99b1ce6c282c43ccf2ef9e25bbd0b230d6
GrooT921 <rarapin96@gmail.com>
2022-10-08 15:45:32 +0200

    gt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
 export const App = () => {
+  const state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  onClickGood () {
+    console.log('click')
+  }
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div>
+        <p>Please leave feedback</p>
+        <button onClick={}>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+      </div>
+      <div>
+        <p>Statistics</p>
+        <ul>
+          <li>Good: { state.good }</li>
+          <li>Neutral: { state.good }</li>
+          <li>Bad: { state.good }</li>
+        </ul>
+      </div>
+    </>
+
   );
 };
